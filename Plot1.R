f7648536761b2bfcca4d6bdeb1aee98b27f5c497
Rick Gilbert <rick.gilbert.net@gmail.com>
2015-03-03 14:05:21 -0800

    Expl. Data Analysis Course Project 1 - March 2015
    Data plotting exercise - 4 R scripts, 4 R plots produced by those
    scripts

---
+++ Plot1.R
@@ -0,0 +1,68 @@
+#################################
+##                              #
+## Exploratory Data Analysis    #
+##                              #
+## Course Project 1, Plot 1     #
+##     histogram                #
+##                              #
+## Rick Gilbert  March 2015     #
+##                              #
+#################################
+
+## The following function was cribbed from R-Help on string capitalization
+capwords <- function(s, strict = FALSE) {
+        cap <- function(s) paste(toupper(substring(s, 1, 1)),
+{s <- substring(s, 2); if(strict) tolower(s) else s},
+sep = "", collapse = " " )
+sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
+}
+
+# Download and/or extract
+if(!file.exists("household_power_consumption.txt")) {
+        if(!file.exists("exdata-data-household_power_consumption.zip")) {
+                download.file(
+                        url = "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                        destfile = "exdata-data-household_power_consumption.zip", mode="wb")
+        }
+        unzip("exdata-data-household_power_consumption.zip")
+}
+
+## Read in the whole data set
+## pwrData <- read.csv2("household_power_consumption.txt")
+pwrData <- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
+
+## data.frame sees the date field as a factor due to dd/mm/yyyy formatting, so coerce
+## the Date field into a date format
+
+pwrData$Date <- as.Date(pwrData$Date,"%d/%m/%Y")
+
+## Now subset the dataframe to retain only the date range 2/1/2007 through 2/2/2007.
+pwrData2 <- subset(pwrData,pwrData$Date == "2007-02-01"|pwrData$Date == "2007-02-02",1:9)
+
+## Now use the Date and Time fields to create a single sampleTime field  
+pwrData2$sampleTime <- strptime(paste(as.character(pwrData2$Date), pwrData2$Time),"%Y-%m-%d %H:%M:%S")
+
+##subset and reorder the data frame, retaining only the columns used in any of the plots
+pwrData3 <- pwrData2[,c(10,3:5,7:9)]
+## pwrData3 <- cbind(pwrData2$sampleTime,data.frame(as.numeric(as.character(pwrData2[,1:6])))
+## pwrData4 <- data.frame(pwrData3)
+
+
+## First plot - histogram of Global_active_power
+
+plotFile <- "plot1R.png"
+if(file.exists(plotFile)){file.remove(plotFile)}
+png(filename = plotFile,
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white", res = NA, family = "", restoreConsole = TRUE,
+    type = c("windows", "cairo", "cairo-png"))
+hist(pwrData3$Global_active_power,
+        main = capwords(gsub("_"," ",names(pwrData3)[2])),
+        xlab = paste(capwords(gsub("_"," ",names(pwrData3)[2])),"(kilowatts)"),
+        col="red")
+dev.off()
+
+
+## And we are done.
+
+
---
+++ Plot4.R
@@ -0,0 +1,90 @@
+#################################
+##                              #
+## Exploratory Data Analysis    #
+##                              #
+## Course Project 1, Plot 4     #
+##  multi-frame plot            #
+##                              #
+## Rick Gilbert  March 2015     #
+##                              #
+#################################
+
+## The following function was cribbed from R-Help on string capitalization
+capwords <- function(s, strict = FALSE) {
+        cap <- function(s) paste(toupper(substring(s, 1, 1)),
+{s <- substring(s, 2); if(strict) tolower(s) else s},
+sep = "", collapse = " " )
+sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
+}
+
+# Download and/or extract
+if(!file.exists("household_power_consumption.txt")) {
+        if(!file.exists("exdata-data-household_power_consumption.zip")) {
+                download.file(
+                        url = "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                        destfile = "exdata-data-household_power_consumption.zip", mode="wb")
+        }
+        unzip("exdata-data-household_power_consumption.zip")
+}
+
+## Read in the whole data set
+## pwrData <- read.csv2("household_power_consumption.txt")
+pwrData <- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
+
+## data.frame sees the date field as a factor due to dd/mm/yyyy formatting, so coerce
+## the Date field into a date format
+
+pwrData$Date <- as.Date(pwrData$Date,"%d/%m/%Y")
+
+## Now subset the dataframe to retain only the date range 2/1/2007 through 2/2/2007.
+pwrData2 <- subset(pwrData,pwrData$Date == "2007-02-01"|pwrData$Date == "2007-02-02",1:9)
+
+## Now use the Date and Time fields to create a single sampleTime field  
+pwrData2$sampleTime <- strptime(paste(as.character(pwrData2$Date), pwrData2$Time),"%Y-%m-%d %H:%M:%S")
+
+##subset and reorder the data frame, retaining only the columns used in any of the plots
+pwrData3 <- pwrData2[,c(10,3:5,7:9)]
+
+
+
+## 4th Plot 4 frame plot
+plotFile <- "plot4R.png"
+if(file.exists(plotFile)){file.remove(plotFile)}
+png(filename = plotFile,
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white", res = NA, family = "", restoreConsole = TRUE,
+    type = c("windows", "cairo", "cairo-png"))
+
+par(mfcol = c(2,2), mar = c(5,4,4,2)) ##,oma=c(1,1,3,1))
+        ## upper left chart
+        plot(pwrData3$sampleTime,pwrData3$Global_active_power, 
+                type = "l",
+                xlab="",
+                ylab=paste(capwords(gsub("_"," ",names(pwrData3)[2]))))
+        ## lower left chart
+        plot(pwrData3$sampleTime,pwrData3$Sub_metering_1,
+                type="n",
+                xlab = "",
+                ylab = "Energy sub metering")
+                lines(pwrData3$sampleTime,pwrData3$Sub_metering_1,type="l",col="black")
+                lines(pwrData3$sampleTime,pwrData3$Sub_metering_2,type="l",col="red")
+                lines(pwrData3$sampleTime,pwrData3$Sub_metering_3,type="l",col="blue")
+                legend("topright",
+                        lty=1,col=c("black","red","blue"),
+                        legend=names(pwrData3)[5:7],
+                        bty = "n")
+        ## upper right chart
+        plot(pwrData3$sampleTime,pwrData3$Voltage, 
+                type = "l",
+                xlab="datetime",
+                ylab=names(pwrData3)[4])
+        ## lower right chart
+        plot(pwrData3$sampleTime,pwrData3$Global_reactive_power, 
+                type = "l",
+                xlab="datetime",
+                ylab=names(pwrData3)[3])        
+dev.off()
+
+## And we are done.
+
+
